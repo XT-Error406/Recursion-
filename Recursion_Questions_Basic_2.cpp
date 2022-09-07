41c9f17092630f3e2aa903d644f53074985c1b2d
Error404 <81263628+XT-Error406@users.noreply.github.com>
2022-09-07 23:01:21 +0530

    Update Recursion_Questions_Basic_2.cpp

--- Recursion_Questions_Basic_2.cpp
+++ Recursion_Questions_Basic_2.cpp
@@ -5,6 +5,96 @@ using namespace std;
 #define yes cout<<"Yes Found "<<el
 #define no  cout<<"NOT Found "<<el
 
+void printocc(int* arr ,int size,int index,int target){
+	if(index==size)return;
+	
+	if(arr[index]==target)cout<<index<<" ";
+	
+	printocc(arr,size,++index,target);
+}
+
+void lastoc1(int* arr, int index,int target){
+	
+	if(index<0){no;return;}
+	
+	if(arr[index]==target){cout<<index;return;}
+	
+	lastoc1(arr,index-1,target);
+}
+
+void lastoc(int* arr,int size,int index,int &ans,int target){
+	
+	if(index==size)return;
+	
+	if(arr[index]==target)ans=index;
+	
+	lastoc(arr,size,index+1,ans,target);
+}
+
+void firstoc2(int* arr, int index,int target,int &ans){
+	if(arr[index]==target){ans=index;return;}
+	
+	 else  firstoc2(arr,index+1,target,ans);
+}
+
+void firstoc1(int* arr ,int &count, int target){
+	
+	if(arr[0]==target)return;
+	
+	
+	  count+=1;
+	  
+	 firstoc1(arr+1,count,target);
+	
+}
+
+int firstoc(int* arr, int size, int index,int target){
+	
+	if(index==size){no;return 0;}
+	
+	if(arr[index]==target)return index;
+	 else firstoc(arr,size,index+1,target);
+}
+
+int maxarray(int* arr ,int size ,int index,int &maxi){
+	
+	if(index==size)return 0;
+	
+	maxi=max(arr[index],maxi);
+	maxarray(arr,size,index+1,maxi);
+	
+	return maxi;
+}
+
+void printarr(int* arr,int size){
+	
+	if(size<0)return;
+	
+	cout<<arr[size]<<" ";
+	
+	printarr(arr,size-1);
+	
+}
+
+int fastexp(int a,int b){
+	
+	if(b==0)return 1; 
+	
+	int sp =fastexp(a ,b/2);
+	
+	if(b&1)return sp*sp*a;
+	
+	else return sp*sp;
+}
+
+int arraymax(int* arr , int size,int index){
+	
+	if(index==size)return INT_MIN;
+	
+	int ans=max(arr[index],arraymax(arr,size,index+1));
+	
+	return ans;
+}
 
 void stringpail(string s,int i,int j){
 	
@@ -102,10 +192,10 @@ int sumcal( int *arr,int size){
  
  int main(){
  	
-	int arr[]={1,2,3,8,9};
+	int arr[]={1,2,9,5,8,9,9};
 	int size=sizeof(arr)/sizeof(arr[0]);
 	int index=0;
-	int target=10;
+	int target=9;
 	//issorted(arr,size,index);
 	//int check=sorted(arr,size);
 	
@@ -124,10 +214,27 @@ int sumcal( int *arr,int size){
 	// }
 	
 	
-	string s="aba";
-	int j =s.length();
+	// string s="aba";
+	// int j =s.length();
 	
-	stringpail(s,0,j-1);
+	// stringpail(s,0,j-1);
 	
+	//cout<<arraymax(arr,size,0);
+	
+	//cout<<fastexp(2,10);
+	
+	//printarr(arr,size-1);
+	 // int maxi=INT_MIN;
+  //  cout<<maxarray(arr,size,0,maxi);
+	
+	int count=0;
+	//firstoc1(arr,count,5);
+	//firstoc2(arr,0,target,count);
+	 int ans=-1;
+	// lastoc(arr,size,0,ans,target);
+	// cout<<ans<<" ";
+	 
+	 //lastoc1(arr,size-1,target);
+	 printocc(arr,size,0,target);
  }
  
