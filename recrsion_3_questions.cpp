1345fe3ed79e97f8a972b932e209f85138559d34
Error404 <81263628+XT-Error406@users.noreply.github.com>
2022-09-15 05:39:43 +0530

    Update recrsion_3_questions.cpp

--- recrsion_3_questions.cpp
+++ recrsion_3_questions.cpp
@@ -4,6 +4,85 @@ using namespace std;
 #define el "\n"
 #define rep(i,a,b) for(int i=a; i<b; i++)
 
+
+
+void equalofBinary_string(char* bin,int i,int j,int leftsum,int rightsum){
+	
+	if(i>j){
+		if(leftsum==rightsum){
+			cout<<bin<<endl;
+		}
+		
+		return;
+	}
+	
+	
+	//four ways---00,01,10,11
+	
+	bin[i]='0';
+	bin[j]='0';
+	
+	equalofBinary_string(bin,i+1,j-1,leftsum,rightsum);
+	
+	bin[i]='0';
+	bin[j]='1';
+	
+	equalofBinary_string(bin,i+1,j-1,leftsum,rightsum+1);
+	
+	
+	bin[i]='1';
+	bin[j]='0';
+	
+	equalofBinary_string(bin,i+1,j-1,leftsum+1,rightsum);
+	
+	bin[i]='1';
+	bin[j]='1';
+	
+	equalofBinary_string(bin,i+1,j-1,leftsum+1,rightsum+1);
+	
+}
+
+// add to string 
+void addstring(string a ,string b , string& ans ,int carry , int i ,int j){
+	
+	if(j<0 && i<0 && carry==0){
+		return;
+	}
+	
+	int first=0;
+	int second=0;
+	
+	if(i>=0){
+		first=a[i]-'0';
+	}
+	
+	if(j>=0)second=b[j]-'0';
+	
+	int sum = first + second + carry;
+	
+	int lastdigit=sum%10;
+	
+	carry=sum/10;
+	
+	ans.push_back(lastdigit + '0');
+
+   addstring(a,b,ans,carry,i-1,j-1);
+}
+
+
+//reverse a string
+void rev(string s,string& ans ,int index){
+	
+	if(index==s.length()){
+	
+		return;
+	}
+	
+	rev(s,ans,index+1);
+        
+    ans.push_back(s[index]);	
+}
+
 //combination of string 
 void comb_string(string s,int index,string output){
 	
@@ -111,7 +190,37 @@ int main(){
 	// int sz=sizeof(set)/sizeof(set[0]);
 	
 	// cout<<subsetsum(set,sum,sz-1);
-	  string s ="abc";
-	  string output="";
-	  comb_string(s,index,output);
+	  // string s ="abcd";
+	  // string output="";
+	 // comb_string(s,index,output);
+	  
+	  // rev(s,output,index);
+	  // cout<<output<<el;
+	  
+	  // string a="25";
+	  // string b="100";
+	  
+	  // string ans="";
+	  
+	  // int carry=0;
+	  
+	  // addstring(a,b,ans,carry,a.length()-1,b.length()-1);
+	  
+	  // reverse(ans.begin(),ans.end());
+	  
+	  // cout<<ans<<el;
+	 
+	  int num;
+	  
+	  cin>>num;
+	  
+	   char* bin =new char[2*num];
+	  int leftsum=0;
+	  int rightsum=0;
+	  int j =2*num-1;
+	  equalofBinary_string(bin,0,j,leftsum,rightsum);
+	 
+	  delete []bin;
+	  
+	
  }
