ac6a4220bba96048b9e4162f5951710a358a9f55
Error404 <81263628+XT-Error406@users.noreply.github.com>
2022-09-17 09:28:21 +0530

    Update K_Symbol_in_Grammer.cpp

--- K_Symbol_in_Grammer.cpp
+++ K_Symbol_in_Grammer.cpp
@@ -1,21 +1,12 @@
 class Solution {
-    private: 
-       int getcount(vector<int>nums,int i,int prev){
-           
-          if(i==nums.size())return 0;
-          
-           int in=0;
-          if(prev==-1 or nums[prev]<nums[i]) in = 1 + getcount(nums,i+1,i); 
-           //prev=index of nums[i] when condn match    
-           int ex= getcount(nums,i+1,prev);
-           
-           return max(in,ex);
-       }
 public:
-    int lengthOfLIS(vector<int>& nums) {
+    int kthGrammar(int n, int k) {
+        if(n==1 || k==1)return 0;
         
-        int prev=-1,i=0;
-        return getcount(nums,i, prev);
-
+         int mid = pow(2,n-1)/2;
+        
+        if(k<=mid)return kthGrammar(n-1,k);
+        else return !kthGrammar(n-1,k-mid);
+    
     }
 };
